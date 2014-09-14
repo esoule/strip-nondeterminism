3127d5d89956ceaca1573fd3d32903add6d41daa
Andrew Ayer <agwa@andrewayer.name>
2014-09-13 21:29:17 -0700

    Add helpers to Zip handler for normalizing members of a Zip archive
    It will be used by Jar handler for normalizing Javadoc files inside
    the Jar.

--- lib/StripNondeterminism/handlers/zip.pm
+++ lib/StripNondeterminism/handlers/zip.pm
@@ -21,13 +21,46 @@ package StripNondeterminism::handlers::zip;
 use strict;
 use warnings;
 
-use Archive::Zip;
+use File::Temp;
+use Archive::Zip qw/:CONSTANTS :ERROR_CODES/;
 
 # A magic number from Archive::Zip for the earliest timestamp that
 # can be represented by a Zip file.  From the Archive::Zip source:
 # "Note, this isn't exactly UTC 1980, it's 1980 + 12 hours and 1
 # minute so that nothing timezoney can muck us up."
 use constant SAFE_EPOCH => 315576060;
+
+# Extract and return the first $nbytes of $member (an Archive::Zip::Member)
+sub peek_member {
+	my ($member, $nbytes) = @_;
+	my $old_compression_method = $member->desiredCompressionMethod(COMPRESSION_STORED);
+	$member->rewindData() == AZ_OK or die "failed to rewind ZIP member";
+	my ($buffer, $status) = $member->readChunk($nbytes);
+	die "failed to read ZIP member" if $status != AZ_OK && $status != AZ_STREAM_END;
+	$member->endRead();
+	$member->desiredCompressionMethod($old_compression_method);
+	return $$buffer;
+}
+
+# Normalize the contents of $member (an Archive::Zip::Member) with $normalizer
+sub normalize_member {
+	my ($member, $normalizer) = @_;
+
+	# Extract the member to a temporary file.
+	my $tempdir = File::Temp->newdir();
+	my $filename = "$tempdir/member";
+	$member->extractToFileNamed($filename);
+
+	# Normalize the temporary file.
+	if ($normalizer->($filename)) {
+		# Normalizer modified the temporary file.
+		# Replace the member's contents with the temporary file's contents.
+		open(my $fh, '<', $filename) or die "Unable to open $filename: $!";
+		$member->contents(do { local $/; <$fh> });
+	}
+
+	unlink($filename);
+}
 
 sub normalize {
 	my ($zip_filename, $filename_cmp) = @_;
