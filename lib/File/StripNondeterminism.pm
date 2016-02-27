461c9ee9589c024531010e7ec57925eb5e9f499c
Reiner Herrmann <reiner@reiner-h.de>
2016-02-27 19:59:57 +0100

    Release 0.016

--- lib/File/StripNondeterminism.pm
+++ lib/File/StripNondeterminism.pm
@@ -33,7 +33,7 @@ use File::StripNondeterminism::handlers::zip;
 
 our($VERSION, $canonical_time);
 
-$VERSION = '0.015'; # 0.015
+$VERSION = '0.016'; # 0.016
 
 sub _get_file_type {
 	my $file=shift;
